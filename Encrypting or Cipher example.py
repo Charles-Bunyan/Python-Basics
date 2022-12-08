2eea24f5ebb2b14d2ad5e3823cbf599209d90b1d
Charles-Bunyan <120106482+Charles-Bunyan@users.noreply.github.com>
2022-12-08 12:57:55 +0000

    Add files via upload

---
+++ Encrypting or Cipher example.py
@@ -0,0 +1,19 @@
+# Encrypting/Cipher
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 3
+newMessage = ''
+
+message = input('Enter a message ')
+
+for character in message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        newPosition = (position + key)% 26
+        newCharacter = alphabet[newPosition]
+        #print('The new character is:', newCharacter)
+        newMessage += newCharacter
+    else:
+        newMessage += character
+
+print('Your new message is ', newMessage)
